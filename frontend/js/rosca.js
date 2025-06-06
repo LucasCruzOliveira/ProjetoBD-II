853b455328471347133afcbc478ca45a8f30a116
Yasmin Sarinho <yasmiinsarinho@gmail.com>
2025-06-06 15:31:47 -0300

    tela de graficos

---
+++ frontend/js/rosca.js
@@ -0,0 +1,51 @@
+const ctx = document.getElementById('graficoRosca').getContext('2d');
+const graficoRosca = new Chart(ctx, {
+    type: 'doughnut',
+    data: {
+      labels: ['Norte', 'CSS', 'JavaScript', 'Java', 'yas'],
+      datasets: [{
+        label: 'Distribuição de Conhecimento',
+        data: [80, 25, 42, 3, 8], // porcentagens ou valores
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56','#ce51ce', '#ec1b5a'],
+        borderColor: '#fff',
+        borderWidth: 2
+      }]
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          position: 'right',
+          labels: {
+            padding: 50
+          }
+        }
+      }
+    }
+});
+
+const ctxPizza = document.getElementById('graficoPizza').getContext('2d');
+  const graficoPizza = new Chart(ctxPizza, {
+    type: 'pie',
+    data: {
+      labels: ['Norte', 'CSS', 'JavaScript', 'Java', 'yas'],
+      datasets: [{
+        label: 'Distribuição de Conhecimento',
+        data: [30, 25, 42, 3, 8], // porcentagens ou valores
+        backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56','#ce51ce', '#ec1b5a'],
+        borderColor: '#fff',
+        borderWidth: 2
+      }]
+    },
+    options: {
+      responsive: false,
+      plugins: {
+        legend: {
+          position: 'right',
+          labels: {
+            padding: 50
+          }
+        }
+      }
+    }
+  });
